c3d93284b803bc65cc26547de81d8ff7ef8d7296
Francisco970-hub <77981948+Francisco970-hub@users.noreply.github.com>
2021-06-15 22:01:39 +0100

    delete admin

--- React-Admin/src/components/dashboard.js
+++
@@ -1,9 +0,0 @@
-import {Card, CardHeader, CardContent} from '@material-ui/core';
-import { Revenue } from './dashboardComponents/revenue'
-export const dashboard = () => (
-  <Revenue />
-  <Card>
-    <CardHeader title="Welcome to the administration" />
-    <CardContent>Text</CardContent>
-  </Card>
-);
